66a96dbfbdb7bf2943afb09c55a44b98a292ce5f
aswath31 <aswathnarayanan007@gmail.com>
2024-12-12 23:14:59 +0530

    Added "response Entity" in controller as initially it was like 200 success but as it is created it should be of 201.

--- BankManagementApp/src/main/java/com/example/bankapp/BankManagementApp/AccountController/AccountController.java
+++ BankManagementApp/src/main/java/com/example/bankapp/BankManagementApp/AccountController/AccountController.java
@@ -3,6 +3,8 @@ package com.example.bankapp.BankManagementApp.AccountController;
 import com.example.bankapp.BankManagementApp.AccountService.AccountService;
 import com.example.bankapp.BankManagementApp.Entity.Account;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,8 @@ public class AccountController {
     @Autowired
     AccountService service;
     @PostMapping("/createAccount")
-    private Account createAccount(@RequestBody Account account){
+    private ResponseEntity<Account> cerateAccount(@RequestBody Account account){
         Account createAccount  =  service.createAccount(account);
-        return createAccount;
+        return ResponseEntity.status(HttpStatus.CREATED).body(createAccount);
     }
 }
